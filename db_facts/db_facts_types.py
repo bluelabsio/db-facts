6f094924aa3864782daced436ea96e910fb397db
Vince Broz <vince.broz@bluelabs.com>
2020-01-20 20:37:35 -0500

    Merge remote-tracking branch 'origin/plugins' into plugin_v2

--- db_facts/db_facts_types.py
+++ db_facts/db_facts_types.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Callable
+from typing import Any, Dict, Callable, Union, Tuple
 
 # note: this file cannot be named 'types.py' or I encounter IDE heartache:
 #
@@ -14,3 +14,5 @@ DBFacts = Dict[str, Any]
 JinjaContext = Dict[str, Any]
 JinjaFilter = Callable[[Any], Any]
 JinjaFilters = Dict[str, JinjaFilter]
+JinjaContextPuller = Callable[[DBName, DBCLIConfig],
+                              Union[JinjaContext, Tuple[JinjaContext, JinjaFilters]]]
