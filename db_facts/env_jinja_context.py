b8f9ef187764fbdb4411e12f43c57d4478e1e104
vinceatbluelabs <vince.broz@bluelabs.com>
2020-02-27 14:02:38 -0500

    Add mypy ratcheting (#17)

--- db_facts/env_jinja_context.py
+++ db_facts/env_jinja_context.py
@@ -1,7 +1,8 @@
 import os
+from .db_facts_types import JinjaContext, DBName, DBCLIConfig
 
 
-def pull_env_jinja_context(db_name, dbcli_config):
+def pull_env_jinja_context(db_name: DBName, dbcli_config: DBCLIConfig) -> JinjaContext:
     return {
         # Returns None if key not found unless default specified
         'getenv': os.getenv,
