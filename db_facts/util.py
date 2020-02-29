f2886fdfb0a61cd83f6b56454179a05b7d8fd988
vinceatbluelabs <vince.broz@bluelabs.com>
2020-02-29 14:34:19 -0500

    Fix incorrect input type annotation on backtick() (#20)
    * Fix incorrect input type annotation on backtick()

--- db_facts/util.py
+++ db_facts/util.py
@@ -1,5 +1,6 @@
+from typing import List, Union
 import subprocess
 
 
-def backtick(cmd: str) -> str:
+def backtick(cmd: Union[str, List[str]]) -> str:
     return subprocess.check_output(cmd).decode('utf-8').strip()
