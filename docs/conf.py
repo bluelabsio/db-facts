8f4d3d2e76a2feb85aaba43121d62113e515306c
Vince Broz <vince.broz@bluelabs.com>
2020-05-16 12:14:11 -0400

    Add source_suffix config

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 
+source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
