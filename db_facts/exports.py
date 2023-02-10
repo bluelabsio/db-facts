5cb803e7ee55cc36b8f0674740ffbc99fc950e69
Bruno Castro-Karney <bruno.castrokarney@bluelabs.com>
2023-02-10 09:09:47 -0800

    Add extra blank lines to appease flake8

--- db_facts/exports.py
+++ db_facts/exports.py
@@ -10,17 +10,21 @@ formatted_key = {
     'database': 'DB_DATABASE',
 }
 
+
 def char_valid(char):
     return char.isalnum() or char == '_'
 
+
 def only_valid_chars(string):
     return ''.join(filter(char_valid, string))
+
 
 def format_key(key):
     if key in formatted_key:
         return formatted_key[key]
 
     return only_valid_chars(key).upper()
+
 
 def print_exports(exports):
     for k, v in sorted(exports.items()):
